0edff78e235decd2370eab72cd56e7ec5b18a2dd
vuphuong1794 <phuongvu1794@gmail.com>
2024-07-28 09:35:44 +0700

    add socketIO

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const {Server} = require("socket.io")
+const http = require("http")
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server)
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+io.on('connection', (socket)=>{
+    console.log("user connected")
+})
+
+server.listen(8000, (req, res)=>{
+    console.log("server is running")
+})
